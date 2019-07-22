995616fe9b55c4d8c4062cadd326c2fa98dbae36
Merve Satmaz <mervesatmaz@gmail.com>
2019-07-22 17:51:04 +0300

    Tolbar tam çalışıyor ilk hali

---
+++ app/src/main/java/com/mervesatmazgmail/forecastmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,19 @@
+package com.mervesatmazgmail.forecastmvvm.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.mervesatmazgmail.forecastmvvm.R
+import com.mervesatmazgmail.forecastmvvm.ui.weather.future.list.FutureListWeatherFragment
+import com.mervesatmazgmail.forecastmvvm.ui.weather.future.list.FutureListWeatherViewModel
+
+class SettingsFragment : Fragment() {
+
+
+
+}
+
